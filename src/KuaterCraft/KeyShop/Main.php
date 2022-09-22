1ba08512aad38ec5be6d760317961b52c5ff9831
KuaterCraft <kuatercraft@gmail.com>
2022-09-22 12:44:02 +0530

    Update Main.php

--- src/KuaterCraft/KeyShop/Main.php
+++ src/KuaterCraft/KeyShop/Main.php
@@ -53,7 +53,7 @@ class Main extends PluginBase implements Listener {
               $player->sendMessage($this->getConfig()->get($data)["Message"]["Failed"]);
             }
       });
-      $money = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->myMoney($player);
+      $myMoney = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->myMoney($player);
       $form->setTitle($this->getConfig()->get("Title"));
       $form->setContent("§4⨠ §eHi, §b" . $player->getName() . "\n§6⨠ §eYour Balance §a" . $mymoney);
       $form->addButton("§l§cExit\n§r§8Tap To Exit", 0, "textures/ui/cancel");
