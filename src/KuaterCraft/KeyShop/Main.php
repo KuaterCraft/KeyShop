7255f2d3c881e136885ec2364b2d6c68a9f6285f
KuaterCraft <kuatercraft@gmail.com>
2022-09-22 13:07:57 +0530

    Update Main.php

--- src/KuaterCraft/KeyShop/Main.php
+++ src/KuaterCraft/KeyShop/Main.php
@@ -54,7 +54,7 @@ class Main extends PluginBase implements Listener {
       });
       $money = EconomyAPI::getInstance()->myMoney($player);
       $form->setTitle($this->getConfig()->get("Title"));
-      $form->setContent("§4⨠ §eHi, §b" . $player->getName() . "\n§6⨠ §eYour Balance §a" . $mymoney);
+      $form->setContent("§4⨠ §eHi, §b" . $player->getName() . "\n§6⨠ §eYour Balance §a" . $myMoney);
       $form->addButton("§l§cExit\n§r§8Tap To Exit", 0, "textures/ui/cancel");
       for($i = 1;$i <= 100;$i++){
           if($this->getConfig()->exists($i)){
